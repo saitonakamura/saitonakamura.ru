57df66f332d7cde7c6f8276240db4ad4f689f310
Saito Nakamura <saitonakamuramb@gmail.com>
2018-02-07 19:00:03 +0100

    Moved to typescript

--- next.config.js
+++ next.config.js
@@ -1,8 +1,30 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-  assetPrefix: '/saitonakamura.ru',
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' },
-    }
+  assetPrefix: !debug ? '/saitonakamura.ru' : '',
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+  webpack: (config, options) => {
+    const { dir, defaultLoaders } = options
+
+    config.resolve.extensions.push('.ts', '.tsx')
+    config.module.rules.push({
+      test: /\.+(ts|tsx)$/,
+      include: [dir],
+      exclude: /node_modules/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: 'awesome-typescript-loader',
+          options: {
+            transpileOnly: true,
+            useTranspileModule: true,
+          },
+        },
+      ],
+    })
+
+    return config
   },
 }
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const Root: React.SFC<{}> = () => (
+  <div>
+    <p>Hello world</p>
+  </div>
+)
+
+export default Root
