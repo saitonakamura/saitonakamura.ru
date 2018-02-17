856204e67a55bcd2536102178f98a6fe4997a61c
Saito Nakamura <saitonakamuramb@gmail.com>
2018-02-18 00:23:15 +0100

    :wrench: Fix of styles export

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,12 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel="stylesheet" href="/_next/static/style.css" />
+          <link
+            rel="stylesheet"
+            href={`${
+              this.props.__NEXT_DATA__.assetPrefix
+            }/_next/static/style.css`}
+          />
         </Head>
         <body>
           <Main />
