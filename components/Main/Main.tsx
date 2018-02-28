54907a60fb88d2f93a1e0e861c548b298a7f1c8a
Saito Nakamura <saitonakamuramb@gmail.com>
2018-03-01 00:48:11 +0100

    :sparkles: Mark up avatar and introduction

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react'
+import avatarUrl from './avatar_squared.jpg'
+import styles from './Main.css'
 
-const Main: React.SFC<{}> = props => (
-  <div {...props}>
-    <img src="/static/avatar_squared.jpg" width="100" />Lola
-  </div>
+const Main: React.SFC<ReactCommonProps & {}> = ({ className, ...rest }) => (
+  <main className={`${className || ''} ${styles.main}`} {...rest}>
+    <picture>
+      <img
+        src={avatarUrl}
+        className={styles.avatar}
+        alt="Avatar of Mikhail Bashurov"
+      />
+    </picture>
+    <div className={styles.introduction}>
+      Hi I’m <span className={styles.name}>Mikhail Bashurov</span>, fullstack
+      developer with React and Dotnet core or Node.js
+    </div>
+  </main>
 )
 
 export default Main
