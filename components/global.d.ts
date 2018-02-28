9f5c2b1a69eac74d1859c800af2a52024e5b82da
Saito Nakamura <saitonakamuramb@gmail.com>
2018-03-01 00:43:08 +0100

    :wrench: Add inline svg loader and image loader

--- components/global.d.ts
+++ components/global.d.ts
@@ -1,9 +1,24 @@
-type ReactCommonComponent = {
+type ReactCommonProps = {
   className?: string
   style?: React.CSSProperties
 }
 
 declare module '*.css' {
   const content: { [key: string]: string }
+  export default content
+}
+
+declare module '*.svg' {
+  const svg: React.SFC<ReactCommonProps & {}>
+  export default svg
+}
+
+declare module '*.png' {
+  const content: string
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: string
   export default content
 }
--- next.config.js
+++ next.config.js
@@ -9,21 +9,51 @@ const typescriptLoaderConfig = {}
 const withTypescript = nextJsConfig =>
   withAwesomeTypescript(typescriptLoaderConfig, nextJsConfig)
 
+const webpackEnrich = (config, options) => {
+  const { dir, dev } = options
+
+  config.module.rules.push(
+    {
+      test: /\.svg$/,
+      include: [dir],
+      loader: '@jonathan.alzetta/react-svg-inline-loader',
+    },
+    {
+      test: /\.(png|jpe?g)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: dev ? '/_next/webpack' : '/_next/static',
+            outputPath: dev ? undefined : '/static',
+          },
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: dev,
+          },
+        },
+      ],
+    },
+  )
+
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+    }),
+  )
+
+  return config
+}
+
 module.exports = withCss(
   withTypescript({
     assetPrefix,
     exportPathMap: () => ({
       '/': { page: '/' },
     }),
-    webpack: config => {
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-        }),
-      )
-
-      return config
-    },
+    webpack: webpackEnrich,
     cssModules: true,
   }),
 )
