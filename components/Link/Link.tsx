5e2a25c1e75f5c6917c2bcba7ad2114f35ab4281
Saito Nakamura <saitonakamuramb@gmail.com>
2018-03-01 00:45:40 +0100

    :bug: Fix issue with links to other sites

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -3,8 +3,18 @@ import * as React from 'react'
 
 const assetPrefix = process.env.ASSET_PREFIX
 
+const urlToCurrentSite = (url: string) =>
+  url.startsWith('/') ||
+  !(url.startsWith('http') || url.startsWith('mailto') || url.startsWith('tel'))
+
 const Link: React.SFC<LinkState> = ({ href, ...rest }) => (
-  <NextJsLink href={href} as={`${assetPrefix}${href}`} {...rest} />
+  <NextJsLink
+    href={href}
+    as={`${
+      urlToCurrentSite((href || '').toString()) ? assetPrefix : ''
+    }${href}`}
+    {...rest}
+  />
 )
 
 export default Link
